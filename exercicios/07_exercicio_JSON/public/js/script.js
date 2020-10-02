7cd2e9970e39a5dbfd94f9a80532c5c45c43e9d5
MatheusMoretti <moretti.matheus.h@gmail.com>
2020-10-02 14:54:23 -0300

    att na escolha do quiz

--- exercicios/07_exercicio_JSON/public/js/script.js
+++ exercicios/07_exercicio_JSON/public/js/script.js
@@ -1,53 +1,68 @@
 var perguntaAtual = 0;
 var numElemento = 0;
+var jogoEscolhido;
 
 function nextQuestion() {
-    var finfo = info;
-    if (finfo[0].jogoEscolhido == "games") { //retornar variavel do json para atribuir a f
-        var f = games;
-    }
-    //var f = finfo.jogoEscolhido; //escolha do usuario na tela principal de qual quiz ele quer
-    var aleatorio;
 
+    //Escolhendo o quiz o escolhido na pagina index
+    //O if impede que ele faça o mesmo trecho mais de 1 vez sendo que nada ali ira mudar
+    if (numElemento < 1) {  
+        let params = new URLSearchParams(location.search);
+        const esc = params.get('p');
+
+        switch (esc) {
+            case "games":
+                jogoEscolhido = games;
+                break;
+        }
+    }
+
+    //---------------------------------------------
+
+    //Numero aleatorio para setar a pergunta de forma a nao repetir
+    var aleatorio;
     do {
         aleatorio = Math.floor((Math.random() * 10)); //fazer verificacao de qual ja foi
-    } while (f[aleatorio] === undefined);
+    } while (jogoEscolhido[aleatorio] === undefined);
 
     //atualizar a var global do qual pergunta estamos 
     perguntaAtual = aleatorio;
     numElemento++;
+
+    //Pergunta do quiz
     document.getElementById('quesTxt').innerHTML = "Você sabe qual 'nome do quiz (ex:jogo)' é esse ? Pergunta " + numElemento + "/10"
-        //Atualizar a imagem da pergunta atual
-    document.getElementById('displayText').innerHTML = "<img src=" + f[aleatorio].link + " width=" + "400px >";
+
+    //Atualizar a imagem da pergunta atual
+    document.getElementById('displayText').innerHTML = "<img src=" + jogoEscolhido[aleatorio].link + " width=" + "400px >";
 
     //Atualizar as respostas de acordo com a atual
-    document.getElementById('res1').innerHTML = f[aleatorio].res1;
-    document.getElementById('res2').innerHTML = f[aleatorio].res2;
-    document.getElementById('res3').innerHTML = f[aleatorio].res3;
-    document.getElementById('res4').innerHTML = f[aleatorio].res4;
+    document.getElementById('res1').innerHTML = jogoEscolhido[aleatorio].res1;
+    document.getElementById('res2').innerHTML = jogoEscolhido[aleatorio].res2;
+    document.getElementById('res3').innerHTML = jogoEscolhido[aleatorio].res3;
+    document.getElementById('res4').innerHTML = jogoEscolhido[aleatorio].res4;
 
 }
 
-var pontuacao = 0; //alterar para json depois
+var pontuacao = 0;
 function qualApertei(id) {
-    var f = games; //seleciona o json
-    var resposta = f[perguntaAtual].resposta; //busca no json a string da resposta correta
+
+    //O json ja foi selecionado no nextQuestion
+    var resposta = jogoEscolhido[perguntaAtual].resposta; //busca no json a string da resposta correta
 
     if (resposta == id) {
         pontuacao++;
-        //adionar ponto de acerto, att json de po
-        console.log(f);
-        console.log(f.length);
+        //adionar ponto de acerto
         document.getElementById('pontuacao').innerHTML = "Pontuacao: " + pontuacao + " / 10";
     }
 
     //Deletando elemento do json para nao haver repeticao
-    delete f[perguntaAtual];
+    delete jogoEscolhido[perguntaAtual];
 
     //Chama a proxima questão
     if (numElemento >= 10) {
         console.log("error");
         //ir para tela de final, pontuacao total e volta para a tela inicial
+        window.location.href = "index.html"
     } else {
         nextQuestion();
     }
@@ -56,10 +71,6 @@ function qualApertei(id) {
 
 //Função para verificar a escolha do jogo acessando o json
 function escolheJogo(escolha) {
-    //Trocar o jogo escolhido no jogo.json
-    var f = info;
-    f.jogoEscolhido = escolha;
-
     //ir para proxima tela
-    window.location.href = "quiz.html";
+    window.location.href = "quiz.html?p=" + escolha; //envia pela url da pagina uma variavel com um valor.
 }
