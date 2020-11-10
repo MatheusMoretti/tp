81a0f60c59c756eeaa03b958dd8743ae6ddb599b
MatheusMoretti <moretti.matheus.h@gmail.com>
2020-11-10 09:17:41 -0300

    Chatbot with backend

---
+++ exercicios/chatbot/routes/ibmWatson.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const ibmWatson = require('../lib/ibmWatsonCredentials');
+
+//post para o servico: ibm watson assistant
+
+router.post('/assistant', function(req, res, next) {
+    console.log("teste de chegada");
+  //recupera a mensagem de texto da conversa
+  let { text, contextDialog } = req.body;
+  let context = JSON.parse(contextDialog);
+
+  //construcao do json para envio dos dados ao servico
+  //input  o texto do usuario, workspaceId seu workspace ID, context o contexto criado
+  let params = {
+      input: { text },
+      workspaceId: 'YOUR WORKSPACE ID', 
+      context
+  }
+
+  //envia os dados ao servico e retorna a mensagem
+  ibmWatson.assistant.message(
+      params,
+      function (err, response){
+          if(err)
+          res.json({ status:'ERRO', data: err.toString() });
+          else
+          res.json({ status:'OK', data:response });
+      }
+  );
+
+});
+
+module.exports = router;
